1d379aa2ccb37bc9116bb7efe9e18793b6e2e5f2
Nastassia Molicheva <nastassia.molicheva@gmail.com>
2019-02-13 18:22:22 -0500

    Update self-port.js
    added pithole shadow

--- self-port.js
+++ self-port.js
@@ -9,13 +9,18 @@ function draw(){
 strokeWeight(8);
 stroke("#8da669");
 fill("#a5c282");
-ellipse(width/2, 350, 240, 300);
+ellipse(width/2, 350, 245, 290);
 
 //Pithole
 strokeWeight(6);
 fill("#8da669");
 noStroke();
-ellipse(width/2, 215, 120, 40);
+ellipse(width/2, 225, 125, 38);
+
+strokeWeight(6);
+fill("#788f5b");
+noStroke();
+ellipse(width/2, 225, 105, 32);
 
 //Eyes
 strokeWeight(6);
@@ -46,13 +51,13 @@ strokeWeight(5);
 stroke("#000");
 ellipse(width/2-55, 340, 15, 15);
 
-//brows
+//Brows
 strokeWeight(8);
 stroke("#8da669");
 line(475, 265, 525, 275);//x,y to hp,vp
 line(370, 275, 420, 265);
 
-//freckles
+//Freckles
 point(515, 380);
 point(495, 370);
 point(400, 380);
@@ -65,17 +70,17 @@ point(455, 382);
 point(455, 382);
 point(472, 360);
 
-//mouth
+//Mouth
 arc(450, 400, 100, 100, 0, PI , CHORD);
 
 
-//arms
+//Arms
 strokeWeight(8);
 stroke("#8da669");
 line((width/2)+125, 350, 650, 300);//x,y to hp,vp
 line((width/2)-125, 350, 250, 300);
 
-//legs
+//Legs
 strokeWeight(8);
 stroke("#8da669");
 line((width/2)+90, 450, 550, 550);//x,y to hp,vp
