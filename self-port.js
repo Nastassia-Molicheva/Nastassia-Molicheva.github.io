c6518f91e70ab96baccb8b9a1a556728893a59ae
Nastassia Molicheva <nastassia.molicheva@gmail.com>
2019-02-13 17:02:52 -0500

    Update self-port.js
    Updates on self-port sketch

--- self-port.js
+++ self-port.js
@@ -12,12 +12,13 @@ fill("#a5c282");
 ellipse(width/2, 350, 240, 300);
 
 //Pithole
-strokeWeight(8);
+strokeWeight(6);
 fill("#8da669");
 noStroke();
 ellipse(width/2, 215, 120, 40);
 
 //Eyes
+strokeWeight(6);
 stroke("#8da669");
 fill("white");
 ellipse(width/2-55, 330, 50, 50)
@@ -36,19 +37,28 @@ noStroke();
 ellipse(width/2-55, 335, 30, 30)
 
 fill("white");
-noStroke();
-ellipse(width/2+50, 335, 10, 10)
+strokeWeight(5);
+stroke("#000");
+ellipse(width/2+55, 340, 15, 15)
 
 fill("white");
-noStroke();
-ellipse(width/2-50, 335, 10, 10)
+strokeWeight(5);
+stroke("#000");
+ellipse(width/2-55, 340, 15, 15)
 
+//brows
+strokeWeight(8);
+stroke("#8da669");
+line(475, 265, 525, 275);//x,y to hp,vp
+line(370, 275, 420, 265);
 
 //mouth
 //fill(white);
 //strokeWeight(8);
 //stroke("black");
 //arc(, 35, 50, 50, 0, -PI, CHORD);  //smile
+//line((width/2)-125, 350, 250, 300);
+
 
 //arms
 strokeWeight(8);
